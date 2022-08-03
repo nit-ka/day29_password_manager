19f61d5d3af8c9c369bcc36128fa6131544a04db
Kinga Nit <nit.kinga@gmail.com>
2022-08-03 11:11:35 +0200

    Adding "search for the password" functionality. Other small changes in the code to improve user's experience.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random
 import json
 
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# PASSWORD GENERATOR
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -19,10 +19,11 @@ def generate_password():
     password_list = password_letters + password_symbols + password_numbers
     random.shuffle(password_list)
     password = "".join(password_list)
+    entry_password.delete(0, END)
     entry_password.insert(0, password)
 
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+# SAVE PASSWORD
 def save():
 
     website = entry_website.get()
@@ -61,7 +62,26 @@ def save():
                 entry_password.delete(0, END)
 
 
-# ---------------------------- UI SETUP ------------------------------- #
+# FIND PASSWORD
+def find_password():
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Password manager - warning", message="No saved passwords in your program yet.")
+    else:
+        searched_website = entry_website.get()
+        try:
+            search_result = messagebox.showinfo(title="Search result",
+                                                message= f"Website: {searched_website}\n"
+                                                         f"Email/Username: {data[searched_website]['email']}\n"
+                                                         f"Password: {data[searched_website]['password']}")
+        except KeyError:
+            messagebox.showinfo(title="Password manager - warning",
+                                message=f"No data for {searched_website} website.")
+
+
+# UI SETUP
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50, bg="white")
@@ -85,23 +105,28 @@ label_password.config(bg="white")
 label_password.grid(column=0, row=3)
 
 # Entries
-entry_website = Entry(width=46)
-entry_website.grid(column=1, row=1, columnspan=2)
+entry_website = Entry(width=32)
+entry_website.grid(column=1, row=1)
 entry_website.focus()
 
-entry_email = Entry(width=46)
-entry_email.grid(column=1, row=2, columnspan=2)
+entry_email = Entry(width=32)
+entry_email.grid(column=1, row=2)
 entry_email.insert(0, "username@gmail.com")
 
-entry_password = Entry(width=28)
+entry_password = Entry(width=32)
 entry_password.grid(column=1, row=3)
 
 # Buttons
+button_search = Button(text="Search", command=find_password)
+button_search.config(width=16)
+button_search.grid(column=2, row=1)
+
 button_generate = Button(text="Generate Password", command=generate_password)
+button_generate.config(width=16)
 button_generate.grid(column=2, row=3)
 
 button_add = Button(text="Add", command=save)
-button_add.config(width=39)
+button_add.config(width=44)
 button_add.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
